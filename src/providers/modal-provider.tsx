d530c78ed21fc4ac3f3806d0405be21f1b22c1d5
pczerox <pczeroxstore@gmail.com>
2023-11-17 04:26:57 -0500

    Edit channel and Delete channel

--- src/providers/modal-provider.tsx
+++ src/providers/modal-provider.tsx
@@ -6,10 +6,12 @@ import { InviteModal } from "@/components/modals/invite-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 
 import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -31,9 +33,9 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
-      {/*
       <DeleteChannelModal />
       <EditChannelModal />
+      {/*
       <MessageFileModal />
       <DeleteMessageModal /> */}
     </>
